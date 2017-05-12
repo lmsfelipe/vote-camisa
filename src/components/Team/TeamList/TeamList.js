c7a77236987b248cc5fba9e87682d0380c1166e2
Felipe Lima <lms.felipe@gmail.com>
2017-05-11 22:10:50 -0300

    TeamList object

--- src/components/Team/TeamList/TeamList.js
+++ src/components/Team/TeamList/TeamList.js
@@ -3,21 +3,24 @@ import TeamItem from './../TeamItem/TeamItem';
 import './TeamList.scss';
 import teamsData from './../../../json/teams';
 
-console.log(teamsData);
-
+console.log(Object.entries(teamsData));
+Object.entries(teamsData).forEach(([key, value], index) => {
+	console.log(key, index);
+})
 
 export default class TeamList extends Component{
 	render(){
 		return(
 			<div className="TeamList">
-				<TeamItem
-	    			teamLogo={teamsData.santos.teamImage}
-					slug={teamsData.santos.slug}
-        		/>
-        		<TeamItem
-	    			teamLogo={teamsData.saoPaulo.teamImage}
-					slug={teamsData.saoPaulo.slug}
-        		/>
+				{
+					Object.entries(teamsData).forEach(([key, value], index) => {
+						console.log(key, value);
+						<TeamItem
+			    			teamLogo={key.teamImage}
+							slug={key.slug}
+			    		/>
+					})					
+				}
 			</div>
 		)
 	}
