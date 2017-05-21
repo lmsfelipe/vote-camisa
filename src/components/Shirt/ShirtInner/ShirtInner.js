e937d632457c180470c3424144f281ce7fcd7d03
Leandro Costa <leandroacosta2008@gmail.com>
2017-05-21 17:56:28 -0300

    vote test

--- src/components/Shirt/ShirtInner/ShirtInner.js
+++ src/components/Shirt/ShirtInner/ShirtInner.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
-import teamsData from './../../../json/teams';
 import ShirtItem from './../ShirtItem/ShirtItem';
 import { Button } from 'react-bootstrap';
 
 export default class ShirtInner extends Component {
   constructor(props) {
     super(props);
-    const { id, slug } = props.params;
-
-    this.state = {
-      slug,
-      id,
-    };
-
-    this.filterShirt = this.filterShirt.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
 
-  filterShirt(shirt) {
-    return shirt.slug === this.state.slug;
+  onClick(e) {
+    const vote = e.target.getAttribute('data-vote');
+    this.props.registerVote(vote);
   }
 
   render() {
     return (
       <div className="ShirtInner">
         {
-          teamsData[this.state.id].shirts.filter(this.filterShirt).map((item, i) =>
+          this.props.teams.map((item, i) =>
             <div key={i} className="ShirtInner__info">
               <ShirtItem
                 shirtName={item.shirtName}
@@ -33,7 +26,7 @@ export default class ShirtInner extends Component {
 
               <p>{item.info}</p>
 
-              <Button bsStyle="info" onClick={this.onClick}>
+              <Button bsStyle="info" data-vote={item.slug} onClick={this.onClick}>
                 Votar nesta camisa
               </Button>
             </div>
--- src/containers/ShirtInnerContainer.js
+++ src/containers/ShirtInnerContainer.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { registerVote } from './../actions/teamActions';
+import teamsData from './../json/teams';
 import ShirtInner from './../components/Shirt/ShirtInner/ShirtInner';
 
 class ShirtInnerContainer extends Component {
-  render() {
-    console.log('container', this.props.params);
+  constructor(props) {
+    super(props);
+    const { id, slug } = props.params;
 
+    this.state = {
+      teams: [],
+      slug,
+      id,
+    };
+
+    this.filterShirt = this.filterShirt.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      teams: teamsData[this.state.id].shirts.filter(this.filterShirt),
+    });
+  }
+
+  filterShirt(shirt) {
+    return shirt.slug === this.state.slug;
+  }
+
+  render() {
     return (
-      <ShirtInner params={this.props.params} />
+      <ShirtInner
+        params={this.props.params}
+        teams={this.state.teams}
+        registerVote={this.props.onClickVote}
+      />
     )
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-  }
+  console.log(state);
+  return {}
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-
+const mapDispatchToProps = (dispatch) => ({
+  onClickVote: (vote) => {
+    dispatch(registerVote(vote))
+  },
 });
 
 export default connect(
