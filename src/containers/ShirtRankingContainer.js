55f918a4d9f3018d090bc59592fdbef4936b36f9
Felipe Lima <fsouza@sciere.com.br>
2017-06-01 18:35:49 -0300

    ranking logic

--- src/containers/ShirtRankingContainer.js
+++ src/containers/ShirtRankingContainer.js
@@ -7,17 +7,6 @@ class ShirtRankingContainer extends Component {
   render(){
     const { globalVote } = this.props;
 
-    let voteSorted
-
-    Object.entries(globalVote).map( (item, i) => {
-      console.log(item[1])
-      let shirt = item[1]
-      voteSorted = Object.keys(shirt).sort( (a,b) => {
-          return shirt[a]-shirt[b]
-      })
-      console.log(voteSorted)
-    })
-
     return (
       <div>
       </div>
@@ -34,3 +23,70 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
 )(ShirtRankingContainer);
+
+
+
+/* Lógica Ordenar votos */
+
+/*
+const votes = {
+    santos: {
+        "2012": {
+            "camisa-a": 34,
+            "camisa-b": 12,
+        },
+        "2013": {
+            "camisa-a": 12,
+            "camisa-b": 67,
+        },
+    },
+    palmeiras: {
+        "2012": {
+            "camisa-a": 56,
+            "camisa-b": 12,
+        },
+        "2013": {
+            "camisa-a": 45,
+            "camisa-b": 67,
+        },
+    },
+    corinthians: {
+        "2012": {
+            "camisa-a": 23,
+            "camisa-b": 12,
+        },
+        "2013": {
+            "camisa-a": 68,
+            "camisa-b": 42,
+        },
+    },
+}
+
+var teamGlobal = new Array();
+console.log(teamGlobal)
+for (time in votes){
+
+    Object.entries(votes[time]).map( ano => {
+        Object.entries(ano[1]).map( camisa => {
+
+            if(!teamGlobal[camisa[1]]) {
+                teamGlobal[camisa[1]] = new Array();
+            }
+
+            if(!teamGlobal[camisa[1]][time]) {
+                teamGlobal[camisa[1]][time] = new Array();
+            }
+
+            if(!teamGlobal[camisa[1]][time][ano[0]]) {
+                teamGlobal[camisa[1]][time][ano[0]] = new Array();
+            }
+            teamGlobal[camisa[1]][time][ano[0]][camisa[0]] = camisa[1];
+        })
+    })
+}
+
+teamGlobal.sort(function(a, b) {
+  return a - b;
+});
+
+console.log(teamGlobal);*/
